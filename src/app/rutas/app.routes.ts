f48ea902c5ceaaeddb8ebadd5618326da4660ee6
KevinMendoza <patricio.mendoza@epn.edu.ec>
2019-08-27 00:04:08 -0500

    Modificacion en los modulos

--- src/app/rutas/app.routes.ts
+++ src/app/rutas/app.routes.ts
@@ -1,26 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
-import { Component } from '@angular/core';
-import { DashboardComponent } from '../pages/dashboard/dashboard.component';
 import { LoginComponent } from '../login/login.component';
-import { ProgressComponent } from '../pages/progress/progress.component';
-import { Graficas1Component } from '../pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from '../shared/nopagefound/nopagefound.component';
-import { PagesComponent } from '../pages/pages.component';
 import { RegisterComponent } from '../login/register.component';
 
 
 // Definicion de rutas
 const appRoutes: Routes = [
-    {
-        path: '',
-        component: PagesComponent,
-        children: [
-            {path: 'dashboard', component: DashboardComponent},
-            {path: 'progress', component: ProgressComponent},
-            {path: 'graficas1', component: Graficas1Component},
-            {path: '', redirectTo: '/dashboard', pathMatch: 'full'}, // cualquier ruta vacia va a redireccionar al dashboard
-        ]
-    },
     {path: 'register', component: RegisterComponent},
     {path: 'login', component: LoginComponent},
     {path: '**', component: NopagefoundComponent }, // cualquier otra ruta que no este definida me muestre el componente page
